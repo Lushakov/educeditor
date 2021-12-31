076078fa99a5727c17ed8b8ac3c9cd1ec2934663
alexreed <p.lushakov@ya.ru>
2021-12-31 12:23:56 +0300

    hjksfgr

---
+++ features/editor/editor.jsx
@@ -0,0 +1,35 @@
+// Import React dependencies.
+import React, { useMemo, useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+
+const Editor = () => {
+    const editor = useMemo(() => withReact(createEditor()), [])
+    const [value, setValue] = useState([
+        {
+            type: 'paragraph',
+            children: [{ text: 'A line of text in a paragraph.' }],
+        },
+    ])
+
+    return (
+        <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+            <Editable
+                onKeyDown={event => {
+                    if (event.key === '&') {
+                        // Prevent the ampersand character from being inserted.
+                        event.preventDefault()
+                        // Execute the `insertText` method when the event occurs.
+                        editor.insertText('and')
+                    }
+                }}
+            />
+        </Slate>
+    )
+}
+
+export default Editor
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Editor from '../features/editor/editor'
+
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <Editor/>
+  )
+}
